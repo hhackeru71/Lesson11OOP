f810f97d73f97217fdf1868e71b21f13c92fe461
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-24 17:57:36 +0200

    .

--- Program.cs
+++ Program.cs
@@ -2,53 +2,77 @@
 {
     internal class Program
     {
+        static void ChangeObjectByAge(Person person)
+        {
+            person.Age = 50;
+        }
+        static void ChangeNum(int num)
+        {
+            num = 2;
+        }
         static void Main(string[] args)
         {
             try
             {
-                Dictionary<int, string> dic = new Dictionary<int, string>();
-                dic.Add(1, "yarin");
-                dic.Add(2, "sdf");
-                dic.Remove(1);// remove by key
+                // value type
+                //int num = 1;
+                //Console.WriteLine(num);
+                //ChangeNum(num);
+                //Console.WriteLine(num);
 
-                Console.WriteLine(dic[1]);
+                //refrence type
+                Person person = new Person();
+                person.Age = 3;
+                Console.WriteLine(person.Age);
+                ChangeObjectByAge(person);
+                Console.WriteLine(person.Age);
 
-                
-                //foreach (var item in dic)
+
+
+
+                //Dictionary<int, string> dic = new Dictionary<int, string>();
+                //dic.Add(1, "yarin");
+                //dic.Add(2, "sdf");
+                //dic.Remove(1);// remove by key
+
+                //Console.WriteLine(dic[1]);
+
+
+                ////foreach (var item in dic)
+                ////{
+                ////    Console.WriteLine(item);
+                ////}
+
+
+
+
+                //List<int> listInt = new List<int>();
+                //listInt.Add(1);
+                //listInt.Add(2);
+                //listInt.Add(3);
+
+                //List<string> listString = new List<string>();
+                //listString.Add("sdf");
+                //listString.Add("yarin");
+                //listString.Add("aa");
+                //listString[0] = "yarin";
+                //listString[1] = "yy";
+
+                //listString.RemoveAt(0);// remove by index
+                //listString.Remove("aa");// remove by item
+                //foreach (var item in listString)
                 //{
                 //    Console.WriteLine(item);
                 //}
 
 
-                
+                //// list int
+                ////listInt[0] = 33;
 
-                List<int> listInt = new List<int>();
-                listInt.Add(1);
-                listInt.Add(2);
-                listInt.Add(3);
-
-                List<string> listString = new List<string>();
-                listString.Add("sdf");
-                listString.Add("yarin");
-                listString.Add("aa");
-                listString[0] = "yarin";
-                listString[1] = "yy";
-
-                listString.RemoveAt(0);// remove by index
-                listString.Remove("aa");// remove by item
-                foreach (var item in listString)
-                {
-                    Console.WriteLine(item);
-                }
-
-
-                // list int
-                //listInt[0] = 33;
-
-                // foreach (var item in listInt)
-                // {
-                //     Console.WriteLine(item);
-                // }
+                //// foreach (var item in listInt)
+                //// {
+                ////     Console.WriteLine(item);
+                //// }
 
             }
             catch (Exception ex)
@@ -59,5 +83,10 @@
 
          
         }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
     }
 }
