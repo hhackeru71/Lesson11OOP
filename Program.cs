b14cbc3b9bc193c5aa496631c54441b931b2d0bd
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-24 17:51:45 +0200

    .

--- Program.cs
+++ Program.cs
@@ -4,33 +4,60 @@
     {
         static void Main(string[] args)
         {
-            List<int> listInt = new List<int>();
-            listInt.Add(1);
-            listInt.Add(2);
-            listInt.Add(3);
-
-            List<string> listString = new List<string>();
-            listString.Add("sdf");
-            listString.Add("yarin");
-            listString.Add("aa");
-            listString[0] = "yarin";
-            listString[1] = "yy";
-
-            listString.RemoveAt(0);// remove by index
-            listString.Remove("aa");// remove by item
-            foreach (var item in listString)
+            try
             {
-                Console.WriteLine(item);
+                Dictionary<int, string> dic = new Dictionary<int, string>();
+                dic.Add(1, "yarin");
+                dic.Add(2, "sdf");
+                dic.Remove(1);// remove by key
+
+                Console.WriteLine(dic[1]);
+
+                
+                //foreach (var item in dic)
+                //{
+                //    Console.WriteLine(item);
+                //}
+
+
+                
+
+                List<int> listInt = new List<int>();
+                listInt.Add(1);
+                listInt.Add(2);
+                listInt.Add(3);
+
+                List<string> listString = new List<string>();
+                listString.Add("sdf");
+                listString.Add("yarin");
+                listString.Add("aa");
+                listString[0] = "yarin";
+                listString[1] = "yy";
+
+                listString.RemoveAt(0);// remove by index
+                listString.Remove("aa");// remove by item
+                foreach (var item in listString)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+                // list int
+                //listInt[0] = 33;
+
+                // foreach (var item in listInt)
+                // {
+                //     Console.WriteLine(item);
+                // }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("error!");
             }
 
-
-            // list int
-            //listInt[0] = 33;
-
-            // foreach (var item in listInt)
-            // {
-            //     Console.WriteLine(item);
-            // }
+         
         }
     }
 }
